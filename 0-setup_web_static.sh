a27c900772031bd2d5f808e16df3edb945bea43b
Kareem1715 <Kareem.H1715@gmail.com>
2024-05-04 01:29:40 +0300

    Task 0 - Setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,15 +7,36 @@ if [ $? -eq 127 ]; then
     apt-get install nginx -y
 fi
 
-mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "This is test for nginx config" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu data
+chown -R ubuntu:ubuntu /data
 
-config_file="/etc/nginx/sites-enabled/default"
-searching="server_name _;"
-new_string="\n\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t}"
-sed -i "s/$searching/$new_string" $config_file
+echo "
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        add_header X-Served-By 438055-web-01;
+        error_page 404 /error_not_found.html;
+        location = /error_not_found.html {
+                root /var/www/html;
+                internal;
+        }
+
+        root /var/www/html;
+        server_name _;
+
+        location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+
+        location /hbnb_static/  {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                alias /data/web_static/current;
+                # try_files $uri $uri/ =404;
+        }
+}" > /etc/nginx/sites-enabled/default
 
 service nginx restart
