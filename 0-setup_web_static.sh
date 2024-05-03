abf2eabda4e7116151a03625deca989a2d7689b6
Kareem1715 <Kareem.H1715@gmail.com>
2024-05-04 01:20:15 +0300

    Task 0 - Setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,15 +7,15 @@ if [ $? -eq 127 ]; then
     apt-get install nginx -y
 fi
 
-mkdir -p /data/web_static/releases
-mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared/
 echo "This is test for nginx config" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu data
 
 config_file="/etc/nginx/sites-enabled/default"
 searching="server_name _;"
-new_string="\n\n\tlocation \/hbnb_static\/ {\n\t\talias /data/web_static/current;\n\t}"
+new_string="\n\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t}"
 sed -i "s/$searching/$new_string" $config_file
 
 service nginx restart
