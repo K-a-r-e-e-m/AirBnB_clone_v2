e87e647240470b6737af1e376a580338d999dd9a
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-11 19:57:38 +0300

    tar files or web_static and compress it to generalte tgz

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Use fabric function do_pack() to generage a tgz archive"""
 from fabric.api import local
 import time
@@ -8,10 +8,9 @@ def do_pack():
     '''generates a .tgz archive from contents of the web_static'''
     # First create versions directpry if doesn't exist
     local("mkdir -p versions")
-    archive_name =  'versions/web_static_' + time.strftime('%Y%m%d%H%M%S') + '.tgz'
+    name = 'versions/web_static_' + time.strftime('%Y%m%d%H%M%S') + '.tgz'
     try:
-        tgz_file = local(f"tar -cvzf {archive_name} web_static")
+        tgz_file = local(f"tar -cvzf {name} web_static")
         return tgz_file
     except Exception:
         return None
-    
