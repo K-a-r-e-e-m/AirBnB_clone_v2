e6f0408f2ddaec31e3f2b8ddaa51baba04db5148
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-11 20:31:49 +0300

    tar files or web_static and compress it to generalte tgz

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,8 +9,8 @@ def do_pack():
     # First create versions directpry if doesn't exist
     local("mkdir -p versions")
     name = 'versions/web_static_' + time.strftime('%Y%m%d%H%M%S') + '.tgz'
-    try:
-        tgz_file = local(f"tar -cvzf {name} web_static")
-        return tgz_file
-    except Exception:
+    result = local(f"tar -cvzf {name} web_static")
+    if result.succeeded:
+        return name
+    else:
         return None
