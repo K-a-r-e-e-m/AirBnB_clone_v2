f8778cbfd1b3e0aa411d55fe637a4efc5ff8a450
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-12 17:14:53 +0300

    Uncompressed tgz file and deploy on server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,17 +11,22 @@ def do_deploy(archive_path):
     '''distributes an archive to my web servers'''
     if not os.path.exists(archive_path):
         return False
-
-    # Get the file name with the .tgz
-    filename = archive_path.split('/')[-1]
-    # archive folder
-    archive = f'/tmp/{filename}'
-    # Get the file name without .tgx
-    without_tgz = f'/data/web_static/releases/{filename.split(".")[0]}'
+    
+    # archive_path = versions/web_static_20170315003959.tgz
+    
+    # filename_tgz = web_static_20170315003959.tgz
+    filename_tgz = archive_path.split('/')[1]
+    
+    # archive = /tmp/web_static_20170315003959.tgz
+    archive = f'/tmp/{filename_tgz}'
+    
+    # filename = web_static_20170315003959
+    filename = filename_tgz.split(".")[0]
+    without_tgz = f'/data/web_static/releases/{filename}'
     try:
-        put(archive_path, '/tmp/')
+        put(archive_path, archive)
         run(f'mkdir -p {without_tgz}/')
-        run(f'tar -xzf {archive} -C {without_tgz}')
+        run(f'tar -xzf {archive} -C {without_tgz}/')
         run(f'rm {archive}')
         run(f'mv {without_tgz}/web_static/* {without_tgz}')
         run(f'rm -rf {without_tgz}/web_static')
