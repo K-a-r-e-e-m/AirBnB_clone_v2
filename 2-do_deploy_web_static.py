9d32d813a0c47a90588e7173b7731e86db5777b1
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-12 22:40:34 +0300

    Depoy web server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,31 +8,23 @@ env.hosts = ['100.26.246.92', '54.237.124.240']
 
 
 def do_deploy(archive_path):
-    '''distributes an archive to my web servers'''
-    if not os.path.exists(archive_path):
-        return False
+   """
+   Deploy archive to web server
+   """
+   try:
+       filename = archive_path.split("/")[-1]
+       no_ext = filename.split(".")[0]
+       path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+       symlink = "/data/web_static/current"
+       put(archive_path, "/tmp/")
+       run("mkdir -p {}".format(path_no_ext))
+       run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
+       run("rm /tmp/{}".format(filename))
+       run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+       run("rm -rf {}web_static".format(path_no_ext))
+       run("rm -rf {}".format(symlink))
+       run("ln -s {} {}".format(path_no_ext, symlink))
+       return True
+   except Exception:
+       return False
 
-    # archive_path = versions/web_static_20170315003959.tgz
-
-    # filename_tgz = web_static_20170315003959.tgz
-    filename_tgz = archive_path.split('/')[-1]
-
-    # archive = /tmp/web_static_20170315003959.tgz
-    archive = f'/tmp/{filename_tgz}'
-
-    # filename = web_static_20170315003959
-    filename = filename_tgz.split(".")[0]
-    without_tgz = f'/data/web_static/releases/{filename}'
-    try:
-        put(archive_path, '/tmp/')
-        run(f'mkdir -p {without_tgz}/')
-        run(f'tar -xzf {archive} -C {without_tgz}/')
-        run(f'rm {archive}')
-        run(f'mv {without_tgz}/web_static/* {without_tgz}')
-        run(f'rm -rf {without_tgz}/web_static')
-        # run(f'rm -rf /data/web_static/current')
-        run(f'ln -s {without_tgz}/ /data/web_static/current')
-        print('New version deployed!')
-        return True
-    except Exception:
-        return False
