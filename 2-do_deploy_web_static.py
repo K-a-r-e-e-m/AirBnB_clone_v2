33dd463ad1210dbff76a7658490aad1a6d400020
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-12 03:18:30 +0300

    Deploy web_static!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,31 +8,18 @@ import os
 env.hosts = ['100.26.246.92', '54.237.124.240']
 
 
-def do_pack():
-    '''generates a .tgz archive from contents of the web_static'''
-    # First create versions directpry if doesn't exist
-    local("mkdir -p versions")
-    name = 'versions/web_static_' + time.strftime('%Y%m%d%H%M%S') + '.tgz'
-    result = local(f"tar -cvzf {name} web_static")
-    if result.succeeded:
-        return name
-    else:
-        return None
-
-
 def do_deploy(archive_path):
     '''distributes an archive to my web servers'''
     if not os.path.exists(archive_path):
         return False
 
+    # Get the file name with the .tgz
+    filename = archive_path.split('/')[1]
+    # archive folder
+    archive = f'/tmp/{filename}'
+    # Get the file name without .tgx
+    without_tgz = f'/data/web_static/releases/{filename.split(".")[0]}'
     try:
-        # Get the file name with the .tgz
-        filename = archive_path.split('/')[1]
-        # archive folder
-        archive = f'/tmp/{filename}'
-        # Get the file name without .tgx
-        without_tgz = f'/data/web_static/releases/{filename.split(".")[0]}'
-
         put(archive_path, '/tmp/')
         sudo(f'mkdir -p {without_tgz}/')
         sudo(f'tar -xzf {archive} -C {without_tgz}')
